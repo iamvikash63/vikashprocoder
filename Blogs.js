091718460b876e3a1d45bb97cd42f8f1c163a9a6
vikashpalgit <palv2540@gmail.com>
2025-05-30 22:38:06 +0530

    changes in blogs section

--- Blogs.js
+++ Blogs.js
@@ -1,5 +1,13 @@
 const blogPosts = [
     {
+      title: "Unique frontend Projects Ideas 2025 - Frontend Web Developer Projects Ideas",
+      description: "Frontend web development can be different types. what comes in your mind when you think about frontend development? frontend means user interface. when you visit any website or app what you see first ? frontend interface. when you are initialy interacting with web or app you interact with its user interface, where you can see the button, images,",
+      image: "Unique frontend project idea.png",
+      link: "Blog-post/Unique-Frontend-Web-Dev-Projects-Ideas.html",
+      Date: "30-05-2025",
+   
+    },
+    {
       title: "How to Earn Money from ChatGPT in 2025 – Ultimate In-Depth Guide",
       description: "The world of artificial intelligence is booming, and at the center of this revolution is ChatGPT, a powerful language model by OpenAI. What many people don’t realize is that ChatGPT is more than just a tool for chatting – it’s a virtual business partner. Whether you’re a student, freelancer, teacher, or entrepreneur, you can tap into the power of AI to generate multiple income streams. In this guide, we’ll explore practical, real-world methods to earn money from ChatGPT – even if you’re starting with zero experience.",
       image: "how to earn from chatgpt img.png",
